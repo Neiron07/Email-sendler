c9f3dd13b4b076a37297a6aa75d95b46ac818561
Neiron07 <neiron@vvv.cash>
2022-10-10 22:37:47 +0600

    email transporter

--- src/helper/sendEmail.js
+++ src/helper/sendEmail.js
@@ -1,2 +1,20 @@
 import * as nodemailer from 'nodemailer';
 
+const transporter = nodemailer.createTransport({
+	host: 'smtp.ethereal.email',
+	port: 587,
+	auth: {
+		user: 'lia.mcclure1@ethereal.email',
+		pass: 'SAguf1ckJbnvHAvKwT'
+	}
+});
+
+let info = await transporter.sendMail({
+	from: '"Fred Foo 👻" <foo@example.com>', // sender address
+	to: 'bar@example.com, baz@example.com', // list of receivers
+	subject: 'Hello ✔', // Subject line
+	text: 'Hello world?', // plain text body
+	html: '<b>Hello world?</b>', // html body
+});
+
+console.log('Message sent: %s', info.messageId);
